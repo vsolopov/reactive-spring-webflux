13acd8a538b4e95513acebd30df26c51d957ef5f
vsolopov <vladyslav.solopov@gmail.com>
2023-05-06 17:37:04 +0300

    change to helper method

--- src/main/java/com/solopov/reactivespringwebflux/service/nasa/NasaPictureService.java
+++ src/main/java/com/solopov/reactivespringwebflux/service/nasa/NasaPictureService.java
@@ -12,6 +12,8 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.StreamSupport;
 
 @Service
 public class NasaPictureService {
@@ -30,10 +32,13 @@ public class NasaPictureService {
                         .queryParam("sol", sol)
                         .build())
                 .exchangeToMono(clientResponse -> clientResponse.bodyToMono(JsonNode.class))
-                .map(jsonNode -> jsonNode.get("photos"))
-                .flatMapMany(Flux::fromIterable)
-                .map(jsonNode -> jsonNode.get("image_src"))
-                .map(JsonNode::asText)
+//               .map(jsonNode -> jsonNode.get("photos"))                                 //previous reactive approach
+//               .flatMapMany(Flux::fromIterable)                                         //previous reactive approach
+//               .map(jsonNode -> jsonNode.get("image_src"))                              //previous reactive approach
+//               .map(JsonNode::asText)                                                   //previous reactive approach
+//                .flatMapMany(jsonNode-> Flux.fromIterable(fetchPictureUrls(jsonNode)))  //1st approach
+                .map(this::fetchPictureUrls)                                              //2nd approach
+                .flatMapMany(Flux::fromIterable)                                          //2nd approach
                 .flatMap(pictureUrl ->
                         WebClient.create(pictureUrl)
                                 .head()
@@ -49,6 +54,12 @@ public class NasaPictureService {
                                 )
                 ).reduce((pic1, pic2)-> pic1.size() > pic1.size() ? pic1 : pic2)
                 .map(Picture::url);
+    }
+
+    private List<String> fetchPictureUrls(JsonNode jsonNode) {
+        return StreamSupport.stream(jsonNode.get("photos").spliterator(), false)
+                .map(photoJson -> photoJson.get("image_src"))
+                .map(JsonNode::asText).toList();
     }
 
 }
